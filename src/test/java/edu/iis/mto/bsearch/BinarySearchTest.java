274d979771ed5b04ea1dbd7bf153450efe2520d4
Kamil Stadryniak <Stadzior@gmail.com>
2016-03-31 18:19:21 +0200

    Added isExceptionThrownWhenSequenceLenghtEqualsZero and isElementFound test methods

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,14 +1,22 @@
 package edu.iis.mto.bsearch;
 
 import static org.junit.Assert.*;
-
+import org.hamcrest.Matchers;
+import static org.hamcrest.Matchers.equalTo;
 import org.junit.Test;
 
 public class BinarySearchTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test(expected = IllegalArgumentException.class)	
+	public void isExceptionThrownWhenSequenceLenghtEqualsZero(){
+		int[] seq = new int[0];
+		SearchResult result = BinarySearch.search(5, seq);
 	}
-
+	
+	@Test
+	public void isElementFound(){
+		int[] seq = new int[5];
+		SearchResult result = BinarySearch.search(5, seq);
+		assertThat(result.isFound(),equalTo(true));
+	}
 }

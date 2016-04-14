d5bf5c18fa85dea0467368748fc26cddb12fb6ea
Stadzior <Stadzior@gmail.com>
2016-04-14 17:27:42 +0200

    First test cases.

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -3,29 +3,50 @@ package edu.iis.mto.bsearch;
 import static org.junit.Assert.*;
 import org.hamcrest.Matchers;
 import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class BinarySearchTest {
 
-	@Test(expected = IllegalArgumentException.class)	
-	public void isExceptionThrownWhenSequenceLenghtEqualsZero(){
-		int[] seq = new int[0];
-		SearchResult result = BinarySearch.search(5, seq);
-	}
-	
+    int[] singleElementSeq;
+    int[] multipleElementSeq;
+
+    @Before public void initialize(){
+        singleElementSeq = new int[]{0};
+        multipleElementSeq = new int[]{1,2,3,4,5};
+    }
+	@Test(expected = IllegalArgumentException.class)
+    public void isExceptionThrown_WhenSequenceLenghtEqualsZero(){
+        SearchResult result = BinarySearch.search(5, singleElementSeq);
+    }
+
 	@Test
 	public void elementFound(){
-		int[] seq = new int[]{1,2,3,4,5};
 		int key = 3;
-		SearchResult result = BinarySearch.search(key, seq);	
-		assertThat(result.isFound() && seq[result.getPosition()] == key ,equalTo(true));
+		SearchResult result = BinarySearch.search(key, multipleElementSeq);
+		assertThat(result.isFound() && multipleElementSeq[result.getPosition()] == key ,equalTo(true));
 	}
 	
 	@Test
 	public void elementNotFound(){
-		int[] seq = new int[]{1,2,3,4};
 		int key = 5;
-		SearchResult result = BinarySearch.search(key, seq);
+		SearchResult result = BinarySearch.search(key, multipleElementSeq);
 		assertThat(!result.isFound() && result.getPosition() == -1 ,equalTo(true));
 	}
+
+    @Test
+    public void foundElementIsFirst(){
+        int key = 1;
+        SearchResult result = BinarySearch.search(key, multipleElementSeq);
+        assertThat(result.isFound() && result.getPosition() == 0 ,equalTo(true));
+    }
+
+    @Test
+    public void foundElementIsLast(){
+        int key = 5;
+        SearchResult result = BinarySearch.search(key, multipleElementSeq);
+        assertThat(result.isFound() && result.getPosition() == multipleElementSeq.length-1 ,equalTo(true));
+    }
+
 }
